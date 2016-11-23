51a32a169726459d2c8b9295073753344ed07f13
Naina Sai Tipparti <tipparti@Nainas-MacBook-Pro.local>
2016-11-23 01:47:20 -0500

    changed

--- app/process.php
+++ app/process.php
@@ -11,7 +11,7 @@ $config["thumbnail_prefix"]			= "thumb_"; //Normal thumb Prefix
 $config["destination_folder"]			= '/img/uploads/'; //upload directory ends with / (slash)
 $config["thumbnail_destination_folder"]		= 'img/uploads/';
  //upload directory ends with / (slash)
-$config["upload_url"] 				= 'http://tipparti.cs518.cs.odu.edu/~ntippart/img/uploads/';
+$config["upload_url"] 				= 'http://tipparti.cs518.cs.odu.edu/img/uploads/';
 $config["quality"] 				= 90; //jpeg quality
 $config["random_file_name"]			= true; //randomize each file name
 
