1419e563ff74e7c28cb08e0becb45771bfd9f576
Naina Sai Tipparti <tipparti@Nainas-MacBook-Pro.local>
2016-12-13 11:46:41 -0500

    Fulltext

--- milestone4dump.sql
+++ milestone4dump.sql
@@ -506,3 +506,11 @@ ALTER TABLE `qa_tagwords`
 ALTER TABLE `qa_titlewords`
   ADD CONSTRAINT `qa_titlewords_ibfk_1` FOREIGN KEY (`postid`) REFERENCES `qa_posts` (`postid`) ON DELETE CASCADE,
   ADD CONSTRAINT `qa_titlewords_ibfk_2` FOREIGN KEY (`wordid`) REFERENCES `qa_words` (`wordid`);
+
+
+  --
+  -- Constraints for table `qa_posts`
+  --
+  ALTER TABLE `qa_posts`
+      ADD FULLTEXT INDEX `IndexName` (`title`)
+      ADD FULLTEXT INDEX `IndexName2` (`content`);
